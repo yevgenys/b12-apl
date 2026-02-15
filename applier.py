4eaf19b77485ca8c6df6a7696c62e1148c62cd56
Ievgenii <4847368+yevgenys@users.noreply.github.com>
2026-02-15 10:23:47 +0100

    validate links are correct

--- applier.py
+++ applier.py
@@ -1,2 +1,8 @@
+import os
+
 if __name__ == '__main__':
-    print("actions works")
+    repo_link = os.environ["REPOSITORY_LINK"]
+    run_link = os.environ["ACTION_RUN_LINK"]
+    
+    print("repo:", repo_link)
+    print("run :", run_link)
